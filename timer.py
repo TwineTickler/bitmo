638e1c4e26283393496e4d59d6e70484bb138de7
twinetickler <sean@Tuvok.local>
2023-06-15 23:32:45 -0500

    determined how long time is from NOW until the most recent API call

--- timer.py
+++ timer.py
@@ -9,6 +9,43 @@ log.check_log_path(False) # If log path doesn't exist, then create it.
 l = 'TIMER: ' # setting log prefix for this file (timer.py)
 log.log(l + '--- STARTING NEW TIMER SESSION ---')
 
+##############################################   Define Functions  #############################################
+
+
+def run_API_program():
+    # Runs the main API get_quote program.
+    # Returns the current Datetime
+
+    with open('run.py') as f:
+        exec(f.read())
+
+    return datetime.now()
+
+
+def format_timedelta(delta) -> str:
+    """Formats a timedelta duration to [N days] %H:%M:%S format"""
+    seconds = int(delta.total_seconds())
+
+    secs_in_a_day = 86400
+    secs_in_a_hour = 3600
+    secs_in_a_min = 60
+
+    days, seconds = divmod(seconds, secs_in_a_day)
+    hours, seconds = divmod(seconds, secs_in_a_hour)
+    minutes, seconds = divmod(seconds, secs_in_a_min)
+
+    time_fmt = f"{hours:02d}:{minutes:02d}:{seconds:02d}"
+
+    if days > 0:
+        suffix = "s" if days > 1 else ""
+        return f"{days} day{suffix} {time_fmt}"
+
+    return time_fmt
+
+
+
+############################################## Begin runtime logic #############################################
+
 # get datetime right now
 now = datetime.now()
 s = 'start time is: {}'.format(now)
@@ -22,17 +59,32 @@ db.create_tables(conn, False) # insure tables are created
 latest_insert_date_list = db.get_latest_response_time(conn)
 
 if (len(latest_insert_date_list) == 1): # we have a valid result from the SQL call
+
     # convert from list/tuple/string into datetime object
+    log.log(l + 'found a valid date as last insert datetime, converting from string to datetime object...')
     latest_insert_date = datetime.strptime(latest_insert_date_list[0][0], '%Y-%m-%d %H:%M:%S.%f')
+
     # print(type(latest_insert_date))
     s = 'The latest insert of quote data was on {}'.format(latest_insert_date)
+    log.log(l + s)
     print(s)
-    
-    # TODO logging
-
 
 else:
+
+    log.log(l + 'no valid last insert date found.')
     s = 'There has never been a successful insert into this database. Running first API call now.'
+    log.log(l + s)
     print(s)
 
-    # TODO logging
+    # run the program
+    latest_insert_date = run_API_program()
+
+# by now we should have a last run datetime. (either just now or sometime)
+
+# first need to determine how far away that run time was from now.
+
+time_since_last_run = (datetime.now() - latest_insert_date)
+s = 'It has been -   {}   - since the last API call'.format(format_timedelta(time_since_last_run))
+log.log(l + s)
+print(s)
+
