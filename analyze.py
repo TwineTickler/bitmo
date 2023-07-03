9a983af63da3ffff9cf1922331d058deb7473171
Sean <sean@iMac.attlocal.net>
2023-07-03 00:40:52 -0500

    completed Response_Status_Groups dict as well as consecutiveGroups List of List

--- analyze.py
+++ analyze.py
@@ -51,9 +51,13 @@
 import config
 import sqlite3
 from datetime import datetime
+from datetime import timedelta
 
 db_prefix = 'bitmo-01'
 db = config.absolute_path + config.db_path + db_prefix
+response_status_grouping_threshold = (60 * 10) # 10 minutes --> in seconds (typically only around 17 - 35 seconds or so apart, depending on which computer is processing them)
+consecutiveDayThreshold = (24 + 8) # in hours --> should be 1 day roughly, but giving 8 hours of buffer.
+consecutiveDays = 5 # how many days would we like to report on?
 
 def open_db_connection():
 
@@ -169,21 +173,121 @@ response_status_groups = {}
 #            NOT SURE IF WE NEED THIS YET --> Order: F (first in group), L (last in group), null (anything else)
 #     AVG: average datetime of insert (middle)
 
-print(result)
+# print(result)
 
-last_row_datetime = 0
+previous_row_insert_date = 0
+current_row_insert_date = 0
 current_group_id = 1
+current_group_insert_dates = []
 
 for row in result:
     print(row)
-    print(row[7])
-    print(type(row[7]))
+    # print(row[7])
+    # print(type(row[7]))
 
-    # these rows will be in order from latest to ealiest
+    # these rows will be in order from oldest to newest
+    current_row_insert_date = convert_str_to_datetime(row[7])
+
+    if previous_row_insert_date == 0:
+
+        # this is the first entry of the list
+        print('first row in the list')
+
+    else:
+        
+        # we have a datetime from the previous row
+        if (previous_row_insert_date + timedelta(seconds=response_status_grouping_threshold)) >= current_row_insert_date:
+
+            # this insert date is within the response_status_grouping_threshold (10 mintutes) of the last insert date (because this loop will be executed in ascending order by insert_date)
+            print('this is in the same group as the previous row')
+
+        else:
+
+            # this is the start of a new group
+            # first build the previous group into the Dictionary
+            # then begin building the next group ID
+            # AND YES, we can actually make this code less lines, by using not's and getting rid of the two else's.. but I think it would be more confusing for a human to read and troubleshoot, so leaving it like this for now.
+            # the first two if statements, essentially do nothing, it's just the else's we are using.
+
+            # add the previous group to the dictionary:
+            print('previous group is DONE, adding it to the dictionary...')
+            # avgTime = datetime.strftime(datetime.fromtimestamp(sum(map(datetime.timestamp,current_group_insert_dates))/len(current_group_insert_dates)),"%H:%M:%S") # I just grabbed this from a post online. Seems to work.
+            avgTime = datetime.fromtimestamp(sum(map(datetime.timestamp,current_group_insert_dates))/len(current_group_insert_dates))  # I just grabbed this from a post online. Seems to work.
+            response_status_groups[current_group_id] = [avgTime,current_group_insert_dates] # TODO need to add the average to this.
+
+            # reset to build the next group ID
+            print('resettings variables and preparing for the next group ID...')
+            current_group_id = current_group_id + 1
+            current_group_insert_dates = []
+            
+    current_group_insert_dates.append(current_row_insert_date)
+    previous_row_insert_date = current_row_insert_date
+
+print('Dictionary complete\n')
+
+for key, value in response_status_groups.items():
+    print('key is: {}'.format(key))
+    print('value is: {}'.format(value))
 
 
 
-    last_row_datetime = convert_str_to_datetime(row[7])
+# Next build the Response Status Group Relationships
+# this will tell us if any day has:
+#     1. a valid day preceeding it.
+#     2. a valid day following it.
 
-    # appending the dictionary
-    response_status_groups[current_group_id] = 
+# scratch that, just going to go straight to the Consecutive Group List
+
+# build the Consecutive Group List.
+# This will show us a groups of Group ID's that fall within the criteria of consecutive days
+#     currently going to define a consecutive day of within the range of 1 day + 8 hours.
+
+consecutiveGroups = []
+
+for groupID, values in response_status_groups.items():
+
+    print(groupID)
+    currentGroupInsertDate = values[0]
+    currentGroupID = groupID
+    currentGroupIDs = [groupID]
+
+    # we only want to execute the while loop IF there are enough days AFTER the current Group ID to check for. So:
+
+    if (len(response_status_groups) - (consecutiveDays - 1)) >= groupID:
+
+        # this will loop through each of the keys in the dictionary (groups/days)
+
+        counter = 1
+
+        while counter < consecutiveDays:
+            
+            # for this group ID, see if we have a consecutive group based off the consecutiveDays we are targeting (ie, 5 days in a row)
+            # if so, create a tuple to add to the consecutive group list
+
+            if (currentGroupInsertDate + timedelta(hours=consecutiveDayThreshold)) >= response_status_groups[(currentGroupID + 1)][0]:
+
+                # this day's insert date is within the threshold from the previous day.
+                
+                counter = counter + 1
+                currentGroupIDs.append(currentGroupID + 1)
+
+                if counter == consecutiveDays:
+                    
+                    # all days passed, add the tuple to the list
+                    consecutiveGroups.append(currentGroupIDs)
+
+                else:
+                    
+                    # update the variables to check for the next day.
+                    currentGroupID = currentGroupID + 1
+                    currentGroupInsertDate = response_status_groups[currentGroupID][0]
+
+            else:
+
+                # one of the days failed, do not add the tuple to the list. End the while loop early.
+                counter = consecutiveDays
+
+print('Consecutive Groups Compiled\n\n')
+for row in consecutiveGroups:
+    print(row)
+        
